2d888b74e2562acdc621cc6b54911ed12b30b975
jesiek <124245821+jesiek@users.noreply.github.com>
2023-10-05 17:16:03 +0200

    5/10/2023 update

---
+++ radar/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Zdarzenia Drogowe</title>
+</head>
+<body>
+<div id="dolny">
+            <div class="klient_pracownik">
+                <h4>Zdarzenia</h4>
+                <form method="POST" action="zdarzenia.php">
+                    <label for="name">Imię</label>
+                    <input type="text" name="imie" /></br>
+                    <label for="surname">Nazwisko</label>
+                    <input type="text" name="nazwisko"></br>
+                </form>
+            </div>
+</body>
+</html>
